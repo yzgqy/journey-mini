67342836063f4576b8b0494474a9d09c020843bd
sakura-wb <37973549+sakura-wb@users.noreply.github.com>
2019-01-18 19:29:49 +0800

    页面更新
    个人中心页面 欢迎页面更新

--- pages/about/about.js
+++ pages/about/about.js
@@ -1,20 +1,76 @@
 // pages/about/about.js
+const app = getApp()
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse) {
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
 
+    var options = [
+      {
+        motto: '个人信息',
+        taptype: 'infoList',
+      },
+      {
+        motto: '游玩记录',
+        taptype: 'historyList',
+      },
+      {
+        motto: '游玩分享',
+        taptype: 'shareList',
+      }
+    ]
+
+    this.setData({
+      options_key: options
+    });
   },
 
+  getUserInfo: function (e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
@@ -62,5 +118,23 @@ Page({
    */
   onShareAppMessage: function () {
 
+  },
+
+    infoList: function (event) {
+    wx.navigateTo({
+      url: './info/info',
+    })
+  },
+
+  historyList: function (event) {
+    wx.navigateTo({
+      url: './history/history',
+    })
+  },
+
+  shareList: function (event) {
+    wx.navigateTo({
+      url: './share/share',
+    })
   }
 })
