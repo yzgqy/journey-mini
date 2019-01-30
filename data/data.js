4c8a4dfe34b8d6af4eb6178281f79658fcfe266a
sakura-wb <37973549+sakura-wb@users.noreply.github.com>
2019-01-30 10:18:48 +0800

    页面更新
    出游列表模板  首页页面（试用）

---
+++ data/data.js
@@ -0,0 +1,36 @@
+var journey_database = [
+  {
+    id: 0,
+    theme: "主题",
+    desc: "描述",
+    startTime: "开始时间",
+    endTime: "结束时间",
+    assemblyPoint: "集合地点",
+    sponsorId: "发起人id",
+    money: "预算",
+    createdAt: "创建时间",
+    updateAt: "更新时间",
+    flag: "是否可用",
+    code: "yaoqingma",
+    isPublic:"公开",
+    isFind:"可查"
+  },
+]
+
+// var info_database = [
+//   {
+//     id:0,
+//     birthday:"1995.04.27",
+//     gender:"男",
+//     desc:"null",
+//     phone:"18851750813",
+//     city:"江苏南京",
+//     school:"南京大学",
+//     createAt:"2019.01.28",
+//     updateAt:"2019.01.28"
+//   },
+// ]
+
+module.exports = {
+  journeyList: journey_database
+}
