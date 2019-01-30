b147d4a4c110aa204be5f1b186eba4fa8289fc31
RabbitLe <39688465+RabbitLe@users.noreply.github.com>
2019-01-30 22:06:19 +0800

    页面更新
    发布页面更新，正在参与列表页面更新

--- pages/join/join.js
+++ pages/join/join.js
@@ -1,31 +1,20 @@
+var journeysData = require('../../data/data.js')
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-    name: "王博",
-    birthday: "1995.04.27",
-    gender: "男",
-    desc: "null",
-    phone: "18851750813",
-    city: "江苏南京",
-    school: "南京大学",
-    createAt: "2019.01.28",
-    updateAt: "2019.01.28"
+  },
+  onLoad: function () {
 
+    this.setData({
+      journeyList: journeysData.journeyList
+    });
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  update: function (event) {
+  onPostTap: function (event) {
+    var journeyId = event.currentTarget.dataset.journeyid;
     wx.navigateTo({
-      url: './join/join',
+      url: "../journey/journey-detail/journey-detail?id=" + journeyId
     })
   },
+
 })
