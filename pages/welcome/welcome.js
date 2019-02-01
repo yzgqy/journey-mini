422d56be26ae5c130c2b9f3338e6caa28b6a565f
yzgqy <772751879@qq.com>
2019-02-01 21:27:08 +0800

    微信登录openid入库

--- pages/welcome/welcome.js
+++ pages/welcome/welcome.js
@@ -50,6 +50,26 @@ Page({//注册当前页面
       userInfo: e.detail.userInfo,
       hasUserInfo: true
     })
+    wx.login({
+      success: function (res) {
+        console.log(res)
+        //获取登录的临时凭证
+        var code = res.code;
+        //调用后端，获取微信的session_key，secret
+        wx.request({
+          url: "https://njuqa.clsaa.com/api/wxLogin?code=" + code,
+          method: "POST",
+          success: function (result) {
+            console.log(result);
+            // 保存用户信息到本地缓存，可以用作小程序端的拦截器
+            // app.setGlobalUserInfo(e.detail.userInfo);
+            // wx.redirectTo({
+            //   url: '../index/index',
+            // })
+          }
+        })
+      }
+    })
   },
 
   onStart(event){
