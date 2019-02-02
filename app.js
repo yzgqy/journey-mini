4b521a52eba284c2e2036b693ac096de344dc36f
mingzihaonanquaaa <46822876+mingzihaonanquaaa@users.noreply.github.com>
2019-02-03 01:59:37 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -1,11 +1,56 @@
 //app.js
 App({//注册小程序
+  globalData:{
+    appid:"wx1549d03167ecb49f",
+    secret: '22dff464f0c121810a1f6319e16b32a7',
+     OPEN_ID : '',
+    SESSION_KEY:'',
+    userId:'190201BNCWDKG9AW'
+
+  },
   onLaunch: function () {
+    var that =this
     // 展示本地存储能力
     var logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
+    var user = wx.getStorageSync('user') || {};
+    var userInfo = wx.getStorageSync('userInfo') || {}; 
 
+    if ((!user.openid || (user.expires_in || Date.now()) < (Date.now() + 600)) && (!userInfo.nickName)) {
+      wx.login({
+        success: function (res) {
+          if (res.code) {
+            wx.getUserInfo({
+              success: function (res) {
+                var objz = {};
+                objz.avatarUrl = res.userInfo.avatarUrl;
+                objz.nickName = res.userInfo.nickName;
+                //console.log(objz);
+                wx.setStorageSync('userInfo', objz);//存储userInfo
+              }
+            });
+            var d = that.globalData;//这里存储了appid、secret、token串  
+            var l = 'https://api.weixin.qq.com/sns/jscode2session?appid=' + d.appid + '&secret=' + d.secret + '&js_code=' + res.code + '&grant_type=authorization_code';
+            wx.request({
+              url: l,
+              data: {},
+              method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT  
+              // header: {}, // 设置请求的 header  
+              success: function (res) {
+                var obj = {};
+                obj.openid = res.data.openid;
+                obj.expires_in = Date.now() + res.data.expires_in;
+                console.log(obj);
+                wx.setStorageSync('user', obj);//存储openid  
+              }
+            });
+          } else {
+            console.log('获取用户登录态失败！' + res.errMsg)
+          }
+        }
+      });
+    }
     // 登录
     wx.login({
       success: res => {
@@ -33,7 +78,4 @@ App({//注册小程序
       }
     })
   },
-  globalData: {
-    userInfo: null
-  }
 })
